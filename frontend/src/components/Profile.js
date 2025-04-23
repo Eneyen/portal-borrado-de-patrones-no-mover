54ab9190d01e78b9cab3cbf6f110ae0f5a53d887
Eneyen <annaysergi@gmail.com>
2025-04-23 21:34:03 +0200

    Añadido Profile.js para evitar error de despliegue

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div>
+      <h2>Perfil del usuario</h2>
+      <p>Este es un componente de ejemplo. Se completará más adelante.</p>
+    </div>
+  );
+}
