fc07eabcde296a109436c5c42ca260716a064b56
Eneyen <annaysergi@gmail.com>
2025-04-23 21:46:24 +0200

    Añadido LanguageSelector.js básico para solucionar error

---
+++ frontend/src/components/LanguageSelector.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function LanguageSelector() {
+  return (
+    <div>
+      <label>Selecciona idioma:</label>
+      <select>
+        <option>Español</option>
+        <option>Inglés</option>
+        <option>Francés</option>
+      </select>
+    </div>
+  );
+}
